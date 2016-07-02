32de091d9c9d89cecf9bbcee67cea174a050d3ea
silver926z <silver926z@gmail.com>
2016-07-02 23:45:31 +0800

    testing

---
+++ Untitled.py
@@ -0,0 +1,5 @@
+Python 2.7.10 (v2.7.10:15c95b7d81dc, May 23 2015, 09:33:12) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
